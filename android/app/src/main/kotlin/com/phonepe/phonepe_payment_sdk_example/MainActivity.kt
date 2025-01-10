a1ad5fb3df9b3e820c15ac336c68662dd954509d
priya.sharma <priya.sharma@phonepe.com>
2025-01-10 15:30:46 +0530

    Added Pgv2 support in sample app

--- android/app/src/main/kotlin/com/phonepe/phonepe_payment_sdk_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/phonepe/phonepe_payment_sdk_example/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.phonepe.phonepe_payment_sdk_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
